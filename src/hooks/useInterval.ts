e35744f571e569a74bacdae21aa99e13dc94f365
Eric So <eric.so.dev@gmail.com>
2022-09-12 00:45:24 -0400

    initial commit

--- src/hooks/useInterval.ts
+++ src/hooks/useInterval.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export function useInterval(callback: () => void, interval: number | null) {
   const saved = useRef<any>();
